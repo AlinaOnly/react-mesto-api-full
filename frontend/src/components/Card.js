1137e3e352467dab70dcee4fb1f81c8689966479
AlinaOnly <kiratrenikova@yandex.ru>
2022-09-19 23:27:53 +0300

    some fix hidden trash button

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -6,7 +6,7 @@ function Card(props) {
   const currentUser = useContext(CurrentUserContext);
   const isOwn = props.card.owner._id === currentUser._id;
   const isLiked = props.card.likes.some(i => i === currentUser._id);
-  const cardDeleteButtonClassName = (`element__trash-button ${isOwn ? "element__trash-button_hidden" : "element__trash-button"}`); 
+  const cardDeleteButtonClassName = (`element__trash-button ${isOwn ? "element__trash-button" : "element__trash-button"}`); 
   const cardLikeButtonClassName = (`element__heart-button ${isLiked ? "element__heart-active" : ""}`);
 
   function handleClick() {
