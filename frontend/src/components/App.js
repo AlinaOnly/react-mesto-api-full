ef9ebe6a72dd4729d26a1b25ed218cc92aaa7458
AlinaOnly <kiratrenikova@yandex.ru>
2022-09-19 15:52:31 +0300

    fix changelike

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -107,12 +107,12 @@ function App() {
   function handleCardLike(card) {
     const isLiked = card.likes.some(i => i._id === currentUser._id);
     if (!isLiked) {
-      api.addLike(card._id, !isLiked).then((newCard) => {
+      api.changeLike(card._id, !isLiked).then((newCard) => {
         setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
       }).catch(err =>
       console.log(err));
     } else {
-      api.deleteLike(card._id, isLiked).then((newCard) => {
+      api.changeLike(card._id, isLiked).then((newCard) => {
         setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
       }).catch(err =>
       console.log(err));
