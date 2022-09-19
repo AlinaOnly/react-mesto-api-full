d2332110f9c0c66cf849e0efaa13cded89533266
AlinaOnly <kiratrenikova@yandex.ru>
2022-09-19 19:02:26 +0300

    liiiike work

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -6,9 +6,9 @@ function Card(props) {
   const currentUser = useContext(CurrentUserContext);
   const isOwn = props.card.owner._id === currentUser._id;
   const cardDeleteButtonClassName = (
-    `element__trash-button ${isOwn ? "element__trash-button" : "element__trash-button_hidden"}`); 
+    `element__trash-button ${isOwn ? "" : "element__trash-button_hidden"}`); 
   const isLiked = props.card.likes.some(i => i._id === currentUser._id);
-  const cardLikeButtonClassName = `element__heart-button ${isLiked && "element__heart-active"}`;
+  const cardLikeButtonClassName = `element__heart-button ${isLiked ? "element__heart-active" : ""}`;
 
   function handleClick() {
     props.onCardClick(props.card);
